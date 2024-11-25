8001e942739fe8d58ad583c79f842688be9e5cbc
Aadya <aaadya_be22@thapar.edu>
2024-11-25 16:29:42 +0530

    error fix

--- index.js
+++ index.js
@@ -18,7 +18,9 @@ app.use(express.urlencoded({ extended: true }));
 // app.use(cors());
 // CORS setup
 // const frontendOrigin = process.env.NODE_ENV === 'production' ? 'https://your-production-frontend-url.com' : 'http://localhost:5173';
-const frontendOrigin = process.env.FRONTEND_URL || 'http://localhost:5173';
+// const frontendOrigin = process.env.FRONTEND_URL || 'http://localhost:5173';
+const frontendOrigin = process.env.FRONTEND_URL;
+
 
 app.use(
   cors({
